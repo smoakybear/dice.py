8809a22932dc66ba70341a17fb167cf5090eb603
smoakybear <53321387+smoakybear@users.noreply.github.com>
2019-07-26 13:03:03 -0400

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,26 @@
+#!python3
+#Random number generator just because
+
+import random
+
+while True:
+    print('Tell me what range you want your random numbers in (enter them one at a time)')
+    myRange1 = int(input())
+    if myRange1 == int:
+        continue
+    print('and')
+    myRange2 = int(input())
+    if myRange2 == int:
+        continue
+    print('How many numbers do you want to generate?')
+    numOfDice = int(input())
+    if numOfDice == int:
+        break
+    
+    for i in range(numOfDice):
+        r = random.randint(myRange1, myRange2)
+    print(r)
+    break
+
+
+# It's so close to working. Just figure out what's wrong with line 20 it's 
